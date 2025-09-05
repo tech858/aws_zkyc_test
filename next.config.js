13e8943473bd297cc0f7f91a87f2af671597c2dd
tech858 <60324191+WIG1234@users.noreply.github.com>
2025-09-05 21:29:39 +0100

    aws

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/sdk',
+  basePath: '/aws',
     async redirects() {
     return [
       {
         source: "/",          // visit http://localhost:3000
-        destination: "/sdk", // go to /site
+        destination: "/aws", // go to /site
         permanent: false,     // 307
         basePath: false,      // 👈 critical: don't auto-add /site
       },
