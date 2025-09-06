8b60063d401803f7bf4d1e4472ac628f290923cd
sohaibsoussi <sohaibsoussi@gmail.com>
2025-09-06 15:32:15 +0100

    edit the routing mechnism

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/aws',
-    async redirects() {
-    return [
-      {
-        source: "/",          // visit http://localhost:3000
-        destination: "/aws", // go to /site
-        permanent: false,     // 307
-        basePath: false,      // 👈 critical: don't auto-add /site
-      },
-    ];
-  },
+  assetPrefix: '/aws',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
